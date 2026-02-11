31314aaabab4c95d69f5f1f478c17977b44a5a8e
GaboRam <vicramfon18@gmail.com>
2026-02-11 13:07:17 -0600

    creando cmb3

--- main.java
+++ main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("repasando git , creando main");
+        System.out.println("repasando git , cambio CMB3");
     }
 }
 
